019bd8d3a3c78e9684b28a572b32acd6fef882ff
Freddy Lau <freddylaudesign@gmail.com>
2024-01-25 10:59:05 -0500

    corrected

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const is_prod = process.env.NODE_ENV === 'production'
-
 // Create a connection  to our mysql database
 const connection = mysql.createConnection({
-    host: process.env.DB_HOSTURL,
-    user: 'root',
-    password: is_prod ? process.env.DB_PASSWORD : '',
-    database: 'mysql_first_day_db'
+    host: process.env.DB_HOST_URL,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE_NAME
   })
   
 
